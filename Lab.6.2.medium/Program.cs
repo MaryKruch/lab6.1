6e9c9729f306080363795f0e0958087d68e7d638
MaryKruch <mariakrucinina041@gmail.com>
2023-12-22 14:53:17 +0200

    first

---
+++ Lab.6.2.medium/Program.cs
@@ -0,0 +1,10 @@
+﻿//1 var
+Console.Write("Введите десятичное число:");
+int n = int.Parse(Console.ReadLine());
+string b = M(n);
+Console.WriteLine($"Десятичное число {n} в двоичной системе: {b}");
+string M(int d)
+{
+    return Convert.ToString(d, 2);
+}
+
